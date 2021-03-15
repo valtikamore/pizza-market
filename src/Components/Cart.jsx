31b004f0d34ebc628b5cd4c20199402a4c7e1ae4
Valentine <valtikaless@gmail.com>
2021-03-15 16:38:39 +0300

    4 useRef / useEffect / useState / BrowserRouter / link

---
+++ src/Components/Cart.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Header from "./Header";
+
+function Cart () {
+    return (
+           <h1>ddd</h1>
+    )
+}
+export default Cart
--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './scss/app.scss';
 import App from './App';
+import {BrowserRouter,Route} from 'react-router-dom'
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <BrowserRouter>
+                <App/>
+        </BrowserRouter>
+
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
